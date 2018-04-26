d877b9232a13cc5883a6a6436a5efed23b569045
Vikram Badarinath <vbad23@gmail.com>
2018-04-25 20:56:18 -0400

    added a blurred image on click. Animation to full name.

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-$(document).ready(function() {
-  $(".about-info").css("display", "flex").hide();
-  $("#about").click(function() {
-    $(".about-info").toggle().fade(500);
+
+var d = new Date();
+var year = d.getFullYear();
+
+// On load
+$( document ).ready(function() {
+  $(".copyright").text("Copyright © " + year + " Vikram Badarinath - All Rights Reserved.");
+
+  // Delayed appearance of full name
+  setTimeout(function() {
+    $(".name").css("opacity", "1");
+    $(".first").css("margin-top", "390px");
+    $(".last").css("margin-top", "292px");
+  }, 2000);
+
+  // Blur background on click
+  $(".item").click(function() {
+    $(".bg").css("background-image", 'url("../images/traverse-bw.jpg")');
+    $(".bg").css("filter", "blur(5px)");
+  });
+
+  // Resets background when initials are clicked
+  $(".initials").click(function() {
+    $(".bg").css("background-image", 'url("../images/traverse.jpg")');
+    $(".bg").css("filter", "blur(0px)");
   });
 });
